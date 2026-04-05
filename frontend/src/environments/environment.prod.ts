c3d377c56cb07f7ca1fc97e48147637061cbcf80
SourabhDohare <sourabhvijaydohare@gmail.com>
2026-04-05 19:20:56 +0530

    adding to vercel and linking

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: '/api/v1'
+  apiUrl: '/api/v1' //'https://YOUR-APP-NAME.onrender.com/api/v1'
 };
